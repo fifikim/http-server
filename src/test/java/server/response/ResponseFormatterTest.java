b769d17d00c5ec97f928609ec1a4b330183cfeca
Sophia Kim <mail@fifikim.com>
2022-07-28 13:25:14 -0400

    update ResponseFormatter tests

--- src/test/java/server/response/ResponseFormatterTest.java
+++ src/test/java/server/response/ResponseFormatterTest.java
@@ -1,52 +1,39 @@
-//package server.response;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import java.util.ArrayList;
-//import org.junit.Test;
-//
-//public class ResponseFormatterTest {
-//  @Test
-//  public void formatsStartLine() {
-//    String expectedStartLine = "HTTP/1.1 200 OK\r\n";
-//    String actualStartLine = ResponseFormatter.startLine("HTTP/1.1 200 OK");
-//
-//    assertEquals(expectedStartLine, actualStartLine);
-//  }
-//
-//  @Test
-//  public void formatsHeaders() {
-//    ArrayList<String> headers = new ArrayList<>();
-//    headers.add("Allow: GET, HEAD, OPTIONS");
-//    headers.add("Content-Length: 143");
-//
-//    String expectedHeaders = "Allow: GET, HEAD, OPTIONS\r\nContent-Length: 143\r\n";
-//    String actualHeaders = ResponseFormatter.headers(headers);
-//
-//    assertEquals(expectedHeaders, actualHeaders);
-//  }
-//
-//  @Test
-//  public void formatsNullHeadersAsEmptyString() {
-//    String expectedHeaders = "";
-//    String actualHeaders = ResponseFormatter.headers(null);
-//
-//    assertEquals(expectedHeaders, actualHeaders);
-//  }
-//
-//  @Test
-//  public void formatsBody() {
-//    String expectedBody = "\r\nHello world";
-//    String actualBody = ResponseFormatter.body("Hello world");
-//
-//    assertEquals(expectedBody, actualBody);
-//  }
-//
-//  @Test
-//  public void formatsNullBodyAsCrlf() {
-//    String expectedBody = "\r\n";
-//    String actualBody = ResponseFormatter.body(null);
-//
-//    assertEquals(expectedBody, actualBody);
-//  }
-//}
+package server.response;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import server.TestHelpers;
+
+public class ResponseFormatterTest {
+  @Test
+  public void correctlyFormatsResponseWithStartLineHeadersAndBody() {
+    Response testResponse = TestHelpers.simpleGetWithBodyResponse();
+
+    String expectedOutput =
+            "HTTP/1.1 200 OK\r\nAllow: GET\r\nContent-Length: 11\r\n\r\nHello world";
+    String actualOutput = ResponseFormatter.toString(testResponse);
+
+    assertEquals(expectedOutput, actualOutput);
+  }
+
+  @Test
+  public void correctlyFormatsResponseWithStartLineAndHeaders() {
+    Response testResponse = TestHelpers.simpleGetResponse();
+
+    String expectedOutput = "HTTP/1.1 200 OK\r\nAllow: GET\r\n\r\n";
+    String actualOutput = ResponseFormatter.toString(testResponse);
+
+    assertEquals(expectedOutput, actualOutput);
+  }
+
+  @Test
+  public void correctlyFormatsResponseWithStartLineOnly() {
+    Response testResponse = TestHelpers.notFoundResponse();
+
+    String expectedOutput = "HTTP/1.1 404 Not Found\r\n\r\n";
+    String actualOutput = ResponseFormatter.toString(testResponse);
+
+    assertEquals(expectedOutput, actualOutput);
+  }
+}
